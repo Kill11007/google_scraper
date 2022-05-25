9a2fa2fcf1393aa7a2ce0d5216b43b179e2c3896
GautamKumar007 <jangragautam98@gmail.com>
2022-05-25 10:57:28 +0530

    Initial Commit

---
+++ src/main/java/com/profilebaba/googledata/GoogleDataApplication.java
@@ -0,0 +1,13 @@
+package com.profilebaba.googledata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/profilebaba/googledata/GoogleDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.profilebaba.googledata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
